902e5864ff07fde069fdcbc12fc210abc62e95c4
takyisky_yt <emmanuelcrash945@gmail.com>
2025-08-01 22:31:12 +0000

    Completed Grade Calculator

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+
+string input;
+int grade;
+
+// Keep looping until right numerical grade is entered.
+while (true)
+{
+    Console.Write("\nEnter your grade (0-100): ");
+    input = Console.ReadLine() ?? "";
+
+    if (int.TryParse(input, out grade))
+    {
+        if (grade >= 0 && grade <= 100)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("!!Grade must be between 0 and 100. Try again.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("!!Invalid input. Please enter a number.");
+    }
+}
+
+// Variable for the letter grade
+string letterGrade;
+
+// Logic to determine the letter grade
+if (grade >= 90)
+{
+    letterGrade = "A";
+}
+else if (grade >= 80)
+{
+    letterGrade = "B";
+}
+else if (grade >= 70)
+{
+    letterGrade = "C";
+}
+else if (grade >= 60)
+{
+    letterGrade = "D";
+}
+else
+{
+    letterGrade = "F";
+}
+
+// Final code output based on logic
+Console.WriteLine("\nThe Letter grade for " + grade + " is " + letterGrade);
+
+
--- GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
+++ GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da2c61d89b8b89e49fe159740208b1964fe93c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e536f2d7c4aa8c53e722ff88f73f7c090e1defa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
